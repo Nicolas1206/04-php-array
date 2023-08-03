ba296b447b4ada133a463d8660911feed85e5e17
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-08-03 14:43:25 +0200

    chore: ex 04

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,33 @@
 <?php
-declare(strict_types=1);
-
-echo 'It works !';
+$family = array('Nicole','Nicolas','David','Miguel');
+print_r($family);
+$favoriteRecipes = array(
+    "Pasta Carbonara",
+    "Chicken Tacos",
+    "Chocolate Cake",
+    "Vegetable Stir-Fry"
+);
+print_r($favoriteRecipes);
+$favoriteFilm = array(
+    "Lord of the rings",
+    "Scary movie",
+    "Pulp fiction",
+);
+$bestFilmIndex = 0;
+echo $favoriteFilm[$bestFilmIndex];
+?>
+<br>
+<?php
+$me = array(
+    'firstname' => 'Nicolas',
+    'lastname' => 'Mateo Uroz',
+    'age' => 28,
+    'city' => 'Liège',
+    'favorite season' => 'summer',
+    'like footbal' => true
+);
+echo '<pre>';
+print_r($me);
+echo '</pre>';
+echo "Like footbal?" . " " . ($me["like football"] ? "No" : "Yes");
+?>
